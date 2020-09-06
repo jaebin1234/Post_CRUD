9c103510406d5bfa26fefcf9d37af250b65cccfd
jaebin1234 <jaebin2586@gmail.com>
2020-09-06 15:35:47 +0900

    init

---
+++ src/main/java/com/example/signuplogin/domain/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.example.signuplogin.domain.repository;
+
+
+import com.example.signuplogin.domain.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    Optional<MemberEntity> findByEmail(String userEmail);
+}
